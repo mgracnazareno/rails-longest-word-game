b56f77ab088c1ed09654b6a7b77ffbb2c668c20d
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-02-11 20:52:27 -0500

    Refactor my random method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,15 +3,19 @@ class GamesController < ApplicationController
   attr_accessor :letters
 
   def new
-    @letters = []
-    random_letters
+    @letters = ('a'..'z').to_a
+    10.times.map { @letters.sample }.join
+    # if params[:letters]
+    #   @letters = @letters.map { |letter| letter.sample.*10.join }
+    # end
   end
 
   def score
   end
 
-  def random_letters
-    @letters = ('a'..'z').to_a
-    10.times.map { @letters.sample }.join
-  end
+  # raise
+  # def random_letters
+  #   @letters = ('a'..'z').to_a
+  #   10.times.map { @letters.sample }.join
+  # end
 end
